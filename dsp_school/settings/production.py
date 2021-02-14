7c085357a7292dee167247f76f0d08c407ecae25
Suraj Mishra <isurajmisra@gmail.com>
2021-02-14 13:05:52 +0530

    added env

--- dsp_school/settings/production.py
+++ dsp_school/settings/production.py
@@ -1,11 +1,16 @@
+from __future__ import absolute_import, unicode_literals
 from .base import *
 import dj_database_url
-
+import os
 
 DEBUG = False
 
-
+env = os.environ.copy()
+SECRET_KEY = env["SECRET_KEY"]
 DATABASES["default"] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 ALLOWED_HOSTS = ['my-wagtail.herokuapp.com']
 
 try:
